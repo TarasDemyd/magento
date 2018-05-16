1c14653c133090f5ebf0034e589ddf4b93645599
Taras <nafaff@gmail.com>
2018-05-16 17:06:53 +0300

    modal added / css changes

--- code/Mage/Testimonials/view/frontend/web/js/module.js
+++ code/Mage/Testimonials/view/frontend/web/js/module.js
@@ -1,6 +1,13 @@
 require(['jquery'], function($) {
-    jQuery('.testimonial-button').click(function () {
+    jQuery('.testimonial-form-button').click(function () {
        jQuery('.testimonials-form-wrapper form').toggle('fast');
+    });
+
+    jQuery('.testimonial-modal-button').click(function () {
+        jQuery('.testimonials-block .login-wrapper').toggle();
+    });
+    jQuery('.testimonials-block .login-container .modal-close').click(function () {
+        jQuery('.testimonials-block .login-wrapper').toggle();
     });
 
 
